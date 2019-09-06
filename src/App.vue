2a967f049136bc6cb6c7b4b009dd79101a2d14bd
zlgz <2352811615@qq.com>
2019-09-06 17:15:10 +0800

    成功引入element-ui日历组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      2019-2020学年校历
+    </div>
+    <calendar></calendar>
+    <router-link to="/" class="copyright">JY项目组</router-link> |
+    <router-link to="/about" class="copyright">掌上建院</router-link>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+  body{
+    background-color: #ffffff;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
+.header{
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  margin: 0 auto 25px auto;
+  text-align: center;
+  font-family: 阿里汉仪智能黑体 ;
+  font-size: 25px;
+  color: #42b983;
+}
+#nav{
+  color: #42b983;
+}
+  .copyright{
+    text-decoration: none;
+    color: #42b983;
+  }
+  div .el-calendar-day{
+    height: 50px;
+  }
 </style>
+<script>
+  import Calendar from "./components/calendar";
+  export default {
+    components: {Calendar}
+  }
+</script>
---
+++ src/components/calendar.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-calendar v-model="value">
+    </el-calendar>
+</template>
+
+<script>
+    export default {
+        name: 'calendar',
+        data() {
+            return {
+                value: new Date()
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
